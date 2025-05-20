1bbebe3655f599b7d34c5feb3ce2da030493b8a3
mariaelisarf <maria.ferreira@sicoobcredimata.com.br>
2025-05-20 14:11:25 -0300

    corrige a inclusão do script do motor do piano e ajusta erro de digitação no CSS

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -4,39 +4,42 @@ const keysCheck = document.querySelector(".keys-check input");
 let mapedKeys = [];
 let audio = new Audio("./src/tunes/a.wav");
 
-const playTune = (key)=>{
-   audio.src = `./src/tunes/${key}.wav`;
+const playTune = (key) => {
+    audio.src = `./src/tunes/${key}.wav`;
     audio.play();
 
     console.log(mapedKeys);
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add("active")
+    setTimeout(() => {
+        clickedKey.classList.remove("active");
+    }, 150);
+
 };
 
-const clickedKey = document.querySelector(`[data-key="${key}"]`);
-clickedKey.classList.add("active")
-setTimeout(() => {
-    clickedKey.classList.remove("active");
-},150);
 
 
 pianoKeys.forEach((key) => {
-    key.addEventListener("click", ()=> playTune(key.dataset.key));
+    key.addEventListener("click", () => playTune(key.dataset.key));
     mapedKeys.push(key.dataset.key);
 });
 
-document.addEventListener("keydown",(e)=>{
-    if(mapedKeyse.key){
-    playTune(e.key);
+document.addEventListener("keydown", (e) => {
+ if (mapedKeys.includes(e.key)) {
+        playTune(e.key);
     }
 });
 
-const handleVolume = (e)=>{
+const handleVolume = (e) => {
     audio.volume = e.target.value;
-
 }
 
-const showHideKeys=()=>{
-    pianoKeys.forEach((key) => {key.classList.toggle("hide")});
+const showHideKeys = () => {
+    pianoKeys.forEach((key) => { key.classList.toggle("hide") });
 }
 
-volumeSlider.addEventListener("input",handleVolume);
-keysCheck.addEventListener("click", showHideKeys);
+document.addEventListener("DOMContentLoaded", () => {
+    volumeSlider.addEventListener("input", handleVolume);
+    keysCheck.addEventListener("click", showHideKeys);
+});
